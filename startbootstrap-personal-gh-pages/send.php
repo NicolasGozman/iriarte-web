8a99fe93756fe164e3f0df15a8d38a68f04544f6
NicolasGozman <nicolas.gozman02@gmail.com>
2023-05-22 23:53:02 -0300

    Second Commit

---
+++ startbootstrap-personal-gh-pages/send.php
@@ -0,0 +1,20 @@
+<?php
+// Recupera los datos del formulario
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$phone = $_POST['Phone'];
+$message = $_POST['Message'];
+
+// Configura los datos del correo electrónico
+$to = 'nicolas.gozman02@gmail.com'; 
+$subject = 'Mensaje de contacto';
+$body = "Nombre: $name\nCorreo electrónico: $email\nTeléfono: $phone\n\nMensaje:\n$message";
+
+// Envía el correo electrónico
+if (mail($to, $subject, $body)) {
+    echo "Correo electrónico enviado con éxito.";
+} else {
+    echo "Error al enviar el correo electrónico.";
+}
+?>
+
